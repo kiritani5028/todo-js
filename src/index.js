f1a29c8e346a9c34611aa0b443f44ab32267ba1f
kiritani5028 <79899556+kiritani5028@users.noreply.github.com>
2022-05-11 17:27:10 +0900

    タスクの追加機能実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する。
+  const text = document.getElementById("add-text");
+  const inputText = text.value;
+  text.value = "";
+  text.focus();
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  //pタグ生成
+  const p = document.createElement("p");
+  p.className = "todo-title";
+  p.textContent = inputText;
+
+  //divタグの子要素に各要素を設定
+  li.appendChild(p);
+
+  //未完了リスに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
