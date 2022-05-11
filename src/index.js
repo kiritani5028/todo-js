fa42bf529efa83e105a461a8bebce2f6513ad9a6
kiritani5028 <79899556+kiritani5028@users.noreply.github.com>
2022-05-11 18:37:11 +0900

    タスク完了機能実装

--- src/index.js
+++ src/index.js
@@ -20,7 +20,23 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.textContent = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    //押された完了ボタンの親タグ(li)を完了リストに追加
+    const completeTarget = completeButton.parentNode;
+    document.getElementById("complete-list").appendChild(completeTarget);
+
+    //buttonタグを削除
+    completeButton.remove();
+    deleteButton.remove();
+
+    //button(戻す)タグ生成
+    const backButton = document.createElement("button");
+    backButton.textContent = "戻す";
+    backButton.addEventListener("click", () => {
+      alert("戻す");
+    });
+
+    //button(戻す)タグをliタグに追加
+    li.appendChild(backButton);
   });
 
   //button(削除)タグ生成
@@ -32,12 +48,12 @@ const onClickAdd = () => {
     deleteTarget.remove();
   });
 
-  //divタグの子要素に各要素を設定
+  //liタグの子要素に各要素を設定
   li.appendChild(p);
   li.appendChild(completeButton);
   li.appendChild(deleteButton);
 
-  //未完了リスに追加
+  //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
 };
 
