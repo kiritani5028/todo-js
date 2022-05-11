14b0c303ca62ebb7719a6eb3df8932d98d6fcc4c
kiritani5028 <79899556+kiritani5028@users.noreply.github.com>
2022-05-11 17:40:34 +0900

    ボタンの追加機能実装

--- src/index.js
+++ src/index.js
@@ -16,8 +16,24 @@ const onClickAdd = () => {
   p.className = "todo-title";
   p.textContent = inputText;
 
+  //button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.textContent = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   //divタグの子要素に各要素を設定
   li.appendChild(p);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
 
   //未完了リスに追加
   document.getElementById("incomplete-list").appendChild(li);
